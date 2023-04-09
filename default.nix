1ff956b8502b9cb3e045c95cda3ac3a88f6f63da
xiashura <plexanov@protonmail.com>
2023-04-09 06:32:49 +0300

    add kind config and scripts in nix-shell

--- default.nix
+++ default.nix
@@ -1,11 +1,21 @@
-with (import <nixpkgs> {});
+with (import <nixpkgs> { });
+let
+  kind-cluster-install = pkgs.writeShellScriptBin "kind-cluster-install" ''
+    kind create cluster --name=k8s-agro-cd-example --config=kind/config.yml
+    kind get kubeconfig --name k8s-agro-cd-example > kube_config.yml 
+  '';
+  kind-cluster-delete = pkgs.writeShellScriptBin "kind-cluster-delete" ''
+    kind delete clusters k8s-agro-cd-example
+  '';
+in stdenv.mkDerivation {
 
-stdenv.mkDerivation {
-
-  KUBECONFIG = "kube_config.yml";
+  KUBECONFIG = "k8s-agro-cd-example";
 
   name = "k8s-simple";
   buildInputs = [
+    kind
+    kind-cluster-install
+    kind-cluster-delete
     terraform
     kubectl
     kind
